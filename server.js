0ad7fd45cf111d610bb5d6f549107b345e1bbe0a
Glitch (cactus-loaf) <none>
2018-07-31 05:56:46 +0000

    📅💒 Checkpoint ./views/index.html:1216088/18 ./server.js:1216088/767 ./package.json:1216088/1018

--- server.js
+++ server.js
@@ -3,14 +3,19 @@
 const express = require('express');
 const cors = require('cors');
 const formidable = require('formidable');
+const fs = require('fs');
 
+
+// const bp = require('body-parser');
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
+// app.use(bp.urlencoded({extended: false}))
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
@@ -19,6 +24,19 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', (req, res) => {
+  const form = formidable.IncomingForm();
+  
+  form.parse(req, (err, fields, files) => {
+    console.log('fields', fields);
+    
+    console.log('files', files);
+    
+    console.log('try', files.upfile);
+  
+  })
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
